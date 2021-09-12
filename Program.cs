41b450acb26f28ae0ce257fb0ea42f710a554de5
WhoIWannaBe <atheismy1998@gmail.com>
2021-09-12 23:25:37 +0800

    First Edition

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using log4net;
+using log4net.Config;
+using System.Net.Http;
+using System.Configuration;
+
+// 這裡的Config，因為不能透過ConfigManager抓，使用相對路徑
+[assembly: log4net.Config.XmlConfigurator(ConfigFile = @"..\..\..\log4net.config", Watch = true)]
+namespace GetTradeClosingDataBatchJob
+{
+    class Program
+    {
+        private static ILog log = LogManager.GetLogger(typeof(Program));        
+        static void Main(string[] args)
+        {            
+
+        }
+    }
+}
