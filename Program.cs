c33feb0687e9396a35c223aa70f5f3cab71c820a
chris_liao <chris_liao@gss.com.tw>
2021-09-13 13:33:41 +0800

    微幅修改

--- Program.cs
+++ Program.cs
@@ -4,6 +4,8 @@ using log4net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using AngleSharp;
+using System.Collections.Generic;
+using System.Configuration;
 
 
 // 這裡的Config，因為不能透過ConfigManager抓，使用相對路徑
@@ -12,9 +14,12 @@ namespace GetTradeClosingDataBatchJob
 {
     class Program
     {
-        private static ILog log = LogManager.GetLogger(typeof(Program));        
+        private static ILog log = LogManager.GetLogger(typeof(Program));
         static async Task Main(string[] args)
         {
+            // 欲抓取的股票代號由Config設定
+            IList<string> StockCodes = new List<string>(ConfigurationManager.AppSettings["FollowStockCodes"].ToString().Split(','));
+
             HttpClient httpClient = new HttpClient();
 
             string url = "https://dannyliu.me";
@@ -23,11 +28,13 @@ namespace GetTradeClosingDataBatchJob
             //檢查回應的伺服器狀態StatusCode是否是200 OK
             if (responseMessage.StatusCode == System.Net.HttpStatusCode.OK)
             {
+                log.Info(string.Format("\n\tUrl:{0}\n\tStatusCode:{1}", url, responseMessage.StatusCode));              
+
                 string responseResult = responseMessage.Content.ReadAsStringAsync().Result;//取得內容
 
                 Console.WriteLine(responseResult);
                 
-                var config = Configuration.Default;
+                var config = AngleSharp.Configuration.Default;
                 var context = BrowsingContext.New(config);
                 var document = await context.OpenAsync(res => res.Content(responseResult));
                 //QuerySelector("head")找出<head></head>元素
